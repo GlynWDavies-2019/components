a76a333a506a4241ed40b00bb00f615dd432442d
Glyn Davies <glynwdavies@gmail.com>
2020-09-03 08:36:46 +0100

    Lecture 36: Added more props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Nadia" />
-            <CommentDetail author="Sydney" />
-            <CommentDetail author="Dixon" />
-            <CommentDetail author="Marshall" />
-            <CommentDetail author="Weiss" />
+            <CommentDetail 
+                author="Nadia" 
+                timeAgo="Today at 4:55PM" 
+                content="Awesome post!" 
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Sydney" 
+                timeAgo="Yesterday at 2:00AM" 
+                content="Totally agree!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Dixon" 
+                timeAgo="Today at 1:30AM" 
+                content="No way!"
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 }
